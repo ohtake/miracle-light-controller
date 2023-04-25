d5dbd51202927b569eb32359c50caf6e70c70b31
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2023-04-25 23:04:18 +0900

    Generate PNG images

---
+++ scripts/generatePng.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import sharp, { format } from "sharp";
+
+const outDir = process.argv[2];
+
+(async () => {
+  const sizes = [192, 512];
+  const input = sharp("static/mlc.svg");
+  await Promise.all(
+    sizes.map((s) =>
+      input
+        .toFormat(format.png)
+        .resize({ width: s, height: s })
+        .toFile(path.join(outDir, `mlc-${s}.png`))
+    )
+  );
+})();
