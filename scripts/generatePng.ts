c4a478d35b34e15e2a79680ab9fabc5707f7b0e4
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2023-04-26 00:03:40 +0900

    Add favicon

--- scripts/generatePng.ts
+++ scripts/generatePng.ts
@@ -4,7 +4,7 @@ import sharp, { format } from "sharp";
 const outDir = process.argv[2];
 
 (async () => {
-  const sizes = [192, 512];
+  const sizes = [32, 192, 512];
   const input = sharp("static/mlc.svg");
   await Promise.all(
     sizes.map((s) =>
